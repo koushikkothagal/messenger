d946c045190741c85ceaf3714e97d3ed84e1bb89
Koushik Kothagal <kkothagal@financialengines.com>
2015-03-14 18:19:20 -0700

    POST method - adding a message

--- src/main/java/org/koushik/javabrains/messenger/resources/MessageResource.java
+++ src/main/java/org/koushik/javabrains/messenger/resources/MessageResource.java
@@ -2,7 +2,9 @@ package org.koushik.javabrains.messenger.resources;
 
 import java.util.List;
 
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
@@ -22,6 +24,14 @@ public class MessageResource {
 		return messageService.getAllMessages();
 	}
 
+	@POST
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.APPLICATION_JSON)
+	public Message addMessage(Message message) {
+		return messageService.addMessage(message);
+	}
+	
+	
 	@GET
 	@Path("/{messageId}")
 	@Produces(MediaType.APPLICATION_JSON)
