d9397f15b60d8dcf0b79635ff3a69b1dcb7a2a2f
Koushik Kothagal <kkothagal@financialengines.com>
2015-03-14 19:54:33 -0700

    Adding update and delete methods

--- src/main/java/org/koushik/javabrains/messenger/resources/MessageResource.java
+++ src/main/java/org/koushik/javabrains/messenger/resources/MessageResource.java
@@ -3,8 +3,10 @@ package org.koushik.javabrains.messenger.resources;
 import java.util.List;
 
 import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
@@ -14,31 +16,40 @@ import org.koushik.javabrains.messenger.model.Message;
 import org.koushik.javabrains.messenger.service.MessageService;
 
 @Path("/messages")
+@Consumes(MediaType.APPLICATION_JSON)
+@Produces(MediaType.APPLICATION_JSON)
 public class MessageResource {
 
 	MessageService messageService = new MessageService();
 	
 	@GET
-	@Produces(MediaType.APPLICATION_JSON)
 	public List<Message> getMessages() {
 		return messageService.getAllMessages();
 	}
 
 	@POST
-	@Consumes(MediaType.APPLICATION_JSON)
-	@Produces(MediaType.APPLICATION_JSON)
 	public Message addMessage(Message message) {
 		return messageService.addMessage(message);
+	}
+	
+	@PUT
+	@Path("/{messageId}")
+	public Message updateMessage(@PathParam("messageId") long id, Message message) {
+		message.setId(id);
+		return messageService.updateMessage(message);
+	}
+	
+	@DELETE
+	@Path("/{messageId}")
+	public void deleteMessage(@PathParam("messageId") long id) {
+		messageService.removeMessage(id);
 	}
 	
 	
 	@GET
 	@Path("/{messageId}")
-	@Produces(MediaType.APPLICATION_JSON)
 	public Message getMessage(@PathParam("messageId") long id) {
-		
 		return messageService.getMessage(id);
-		
 	}
 	
 }
