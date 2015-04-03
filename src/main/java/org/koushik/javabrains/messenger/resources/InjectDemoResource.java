61673b6807baf891d9b414641cce7378259ee88c
Koushik Kothagal <kkothagal@financialengines.com>
2015-04-03 11:54:58 -0700

    Using more Param annotations

---
+++ src/main/java/org/koushik/javabrains/messenger/resources/InjectDemoResource.java
@@ -0,0 +1,26 @@
+package org.koushik.javabrains.messenger.resources;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.CookieParam;
+import javax.ws.rs.GET;
+import javax.ws.rs.HeaderParam;
+import javax.ws.rs.MatrixParam;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+@Path("/injectdemo")
+@Consumes(MediaType.TEXT_PLAIN)
+@Produces(MediaType.TEXT_PLAIN)
+public class InjectDemoResource {
+	
+	@GET
+	@Path("annotations")
+	public String getParamsUsingAnnotations(@MatrixParam("param") String matrixParam,
+											@HeaderParam("authSessionID") String header,
+											@CookieParam("name") String cookie) {
+		return "Matrix param: " + matrixParam + " Header param: " + header + " Cookie param: " + cookie;
+	}
+	
+
+}
