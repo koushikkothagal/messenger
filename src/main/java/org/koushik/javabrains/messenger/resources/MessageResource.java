4ecca03a0ea1c5ebe96fdb8cdd1875b59ea12970
Koushik Kothagal <kkothagal@financialengines.com>
2015-04-03 11:10:56 -0700

    Implemented pagination and filtering for messages API

--- src/main/java/org/koushik/javabrains/messenger/resources/MessageResource.java
+++ src/main/java/org/koushik/javabrains/messenger/resources/MessageResource.java
@@ -10,6 +10,7 @@ import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 
 import org.koushik.javabrains.messenger.model.Message;
@@ -23,7 +24,15 @@ public class MessageResource {
 	MessageService messageService = new MessageService();
 	
 	@GET
-	public List<Message> getMessages() {
+	public List<Message> getMessages(@QueryParam("year") int year,
+									 @QueryParam("start") int start,
+									 @QueryParam("size") int size) {
+		if (year > 0) {
+			return messageService.getAllMessagesForYear(year);
+		}
+		if (start >= 0 && size > 0) {
+			return messageService.getAllMessagesPaginated(start, size);
+		}
 		return messageService.getAllMessages();
 	}
 
--- src/main/java/org/koushik/javabrains/messenger/service/MessageService.java
+++ src/main/java/org/koushik/javabrains/messenger/service/MessageService.java
@@ -1,6 +1,7 @@
 package org.koushik.javabrains.messenger.service;
 
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.List;
 import java.util.Map;
 
@@ -19,6 +20,25 @@ public class MessageService {
 	public List<Message> getAllMessages() {
 		return new ArrayList<Message>(messages.values()); 
 	}
+	
+	public List<Message> getAllMessagesForYear(int year) {
+		List<Message> messagesForYear = new ArrayList<>();
+		Calendar cal = Calendar.getInstance();
+		for (Message message : messages.values()) {
+			cal.setTime(message.getCreated());
+			if (cal.get(Calendar.YEAR) == year) {
+				messagesForYear.add(message);
+			}
+		}
+		return messagesForYear;
+	}
+	
+	public List<Message> getAllMessagesPaginated(int start, int size) {
+		ArrayList<Message> list = new ArrayList<Message>(messages.values());
+		if (start + size > list.size()) return new ArrayList<Message>();
+		return list.subList(start, start + size); 
+	}
+	
 	
 	public Message getMessage(long id) {
 		return messages.get(id);
